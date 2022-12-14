548b1c8aa27cb17467119a4099cec52e72394e06
yunmoya <li_cuican@163.com>
2022-12-14 17:24:17 +0800

    modify chaincode to fix mvcc conflicts

--- chaincode/atcc/atcc.go
+++ chaincode/atcc/atcc.go
@@ -14,114 +14,99 @@ type SmartContract struct {
 	contractapi.Contract
 }
 
-// Asset describes basic details of what makes up a simple asset
-type Asset struct {
+type ECAsset struct {
 	ID               string `json:"ID"`
 	EndorsementCount int    `json:"EndorsementCount"`
-	Status           int    `json:"Status"`
+}
+
+type StatusAsset struct {
+	ID     string `json:"ID"`
+	Status int    `json:"Status"`
 }
 
 func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
-	assets := []Asset{
-		{ID: "asset1-0", EndorsementCount: 1, Status: 0},
-		{ID: "asset1-1", EndorsementCount: 1, Status: 0},
-		{ID: "asset1-2", EndorsementCount: 1, Status: 0},
-		{ID: "asset2-0", EndorsementCount: 1, Status: 0},
-		{ID: "asset2-1", EndorsementCount: 1, Status: 0},
-	}
-
-	for _, asset := range assets {
-		assetJSON, err := json.Marshal(asset)
-		if err != nil {
-			return err
-		}
-
-		err = ctx.GetStub().PutState(asset.ID, assetJSON)
-		if err != nil {
-			return fmt.Errorf("failed to put to world state. %v", err)
-		}
-	}
-
 	return nil
 }
 
-func (s *SmartContract) CreateLedger(ctx contractapi.TransactionContextInterface, id string) error {
-	exists, err := s.AssetExists(ctx, id)
+func (s *SmartContract) ReadEC(ctx contractapi.TransactionContextInterface, id string) (*ECAsset, error) {
+	resultIterator, err := ctx.GetStub().GetStateByPartialCompositeKey("id~ec~txID", []string{id})
 	if err != nil {
-		return err
+		return nil, fmt.Errorf("Could not retrieve value for %s: %s", id, err.Error())
 	}
-	if exists {
-		return fmt.Errorf("the asset %s already exists", id)
+	defer resultIterator.Close()
+
+	// Check the variable existed
+	if !resultIterator.HasNext() {
+		return nil, fmt.Errorf("No variable by the id %s exists", id)
 	}
 
-	asset := Asset{
+	// Iterate through result set and compute final ec
+	ec := 0
+	for i := 0; resultIterator.HasNext(); i++ {
+		// Get the next row
+		responseRange, err := resultIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+
+		// Split the composite key into its component parts
+		_, keyParts, err := ctx.GetStub().SplitCompositeKey(responseRange.GetKey())
+		if err != nil {
+			return nil, err
+		}
+
+		// Retrieve the delta value and operation
+		curEcStr := keyParts[1]
+
+		// Convert the value string and perform the operation
+		curEc, err := strconv.Atoi(curEcStr)
+		if err != nil {
+			return nil, err
+		}
+		ec += curEc
+	}
+
+	asset := ECAsset{
 		ID:               id,
-		EndorsementCount: 1,
-		Status:           0,
-	}
-
-	assetJSON, err := json.Marshal(asset)
-	if err != nil {
-		return err
-	}
-
-	return ctx.GetStub().PutState(id, assetJSON)
-}
-
-func (s *SmartContract) ReadAsset(ctx contractapi.TransactionContextInterface, id string) (*Asset, error) {
-	assetJSON, err := ctx.GetStub().GetState(id)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read from world state: %v", err)
-	}
-	if assetJSON == nil {
-		return nil, fmt.Errorf("the asset %s does not exist", id)
-	}
-
-	var asset Asset
-	err = json.Unmarshal(assetJSON, &asset)
-	if err != nil {
-		return nil, err
+		EndorsementCount: ec,
 	}
 
 	return &asset, nil
 }
 
 func (s *SmartContract) EndorsementInc(ctx contractapi.TransactionContextInterface, id string) error {
-	exists, err := s.AssetExists(ctx, id)
+	// Retrieve info needed for the update procedure
+	txid := ctx.GetStub().GetTxID()
+	compositeIndexName := "id~ec~txID"
+
+	// Create the composite key that will allow us to query for all deltas on a particular variable
+	comopositeKey, err := ctx.GetStub().CreateCompositeKey(compositeIndexName, []string{id, "1", txid})
 	if err != nil {
-		return err
-	}
-	if !exists {
-		return fmt.Errorf("the asset %s does not exists", id)
+		return fmt.Errorf("Could not create a composite key for %s: %s", id, err.Error())
 	}
 
-	curAsset, err := s.ReadAsset(ctx, id)
+	// Save the composite key index
+	err = ctx.GetStub().PutState(comopositeKey, []byte{0x00})
 	if err != nil {
-		return err
+		return fmt.Errorf("Could not put operation for %s in the ledger: %s", id, err.Error())
 	}
 
-	curAsset.EndorsementCount++
-	assetJSON, err := json.Marshal(curAsset)
-	if err != nil {
-		return err
-	}
-
-	return ctx.GetStub().PutState(id, assetJSON)
+	return nil
 }
 
-func (s *SmartContract) DeleteAsset(ctx contractapi.TransactionContextInterface, id string) error {
-	exists, err := s.AssetExists(ctx, id)
-	if err != nil {
-		return err
-	}
-	if !exists {
-		return fmt.Errorf("the asset %s does not exist", id)
-	}
+//func (s *SmartContract) DeleteAsset(ctx contractapi.TransactionContextInterface, id string) error {
+//	exists, err := s.AssetExists(ctx, id)
+//	if err != nil {
+//		return err
+//	}
+//	if !exists {
+//		return fmt.Errorf("the asset %s does not exist", id)
+//	}
+//
+//	return ctx.GetStub().DelState(id)
+//}
 
-	return ctx.GetStub().DelState(id)
-}
-
-func (s *SmartContract) AssetExists(ctx contractapi.TransactionContextInterface, id string) (bool, error) {
+func (s *SmartContract) StatusAssetExists(ctx contractapi.TransactionContextInterface, id string) (bool, error) {
 	assetJSON, err := ctx.GetStub().GetState(id)
 	if err != nil {
 		return false, fmt.Errorf("failed to read from world state: %v", err)
@@ -130,21 +115,20 @@ func (s *SmartContract) AssetExists(ctx contractapi.TransactionContextInterface,
 	return assetJSON != nil, nil
 }
 
-func (s *SmartContract) ChangeStatus(ctx contractapi.TransactionContextInterface, id string, status int) error {
-	exists, err := s.AssetExists(ctx, id)
+func (s *SmartContract) SetStatus(ctx contractapi.TransactionContextInterface, id string) error {
+	exists, err := s.StatusAssetExists(ctx, id)
 	if err != nil {
 		return err
 	}
-	if !exists {
-		return fmt.Errorf("the asset %s does not exist", id)
-	}
-	curAsset, err := s.ReadAsset(ctx, id)
-	if err != nil {
-		return err
+	if exists {
+		return fmt.Errorf("The StatusAsset %s existed", id)
 	}
 
-	curAsset.Status = status
-	assetJSON, err := json.Marshal(curAsset)
+	asset := StatusAsset{
+		ID:     id,
+		Status: 1,
+	}
+	assetJSON, err := json.Marshal(asset)
 	if err != nil {
 		return err
 	}
@@ -152,33 +136,24 @@ func (s *SmartContract) ChangeStatus(ctx contractapi.TransactionContextInterface
 	return ctx.GetStub().PutState(id, assetJSON)
 }
 
-func (s *SmartContract) GetAllAssets(ctx contractapi.TransactionContextInterface) ([]*Asset, error) {
+func (s *SmartContract) GetAllAssets(ctx contractapi.TransactionContextInterface) ([]string, error) {
 	resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
 	if err != nil {
 		return nil, err
 	}
 	defer resultsIterator.Close()
 
-	var assets []*Asset
+	var results []string
 	for resultsIterator.HasNext() {
 		queryResponse, err := resultsIterator.Next()
 		if err != nil {
 			return nil, err
 		}
 
-		var asset Asset
-		err = json.Unmarshal(queryResponse.Value, &asset)
-		if err != nil {
-			return nil, err
-		}
-		assets = append(assets, &asset)
+		results = append(results, string(queryResponse.Value))
 	}
 
-	return assets, nil
-}
-
-func getId(blockNumber uint64, offset int) string {
-	return "asset" + strconv.FormatUint(blockNumber, 10) + "-" + strconv.Itoa(offset)
+	return results, nil
 }
 
 func main() {
